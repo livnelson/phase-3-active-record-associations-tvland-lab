c1d8de8b99e60b3b65afd7299ef20939ac2baed1
Liv Nelson <livnelson@me.com>
2022-11-29 21:33:24 -0800

    commit tree 1dec4ef1e39911b3fc7f078c2baae3cc8a781e69

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 class Actor < ActiveRecord::Base
-  
-end
+  has_many :characters
+  has_many :shows, through: :characters
+
+    def full_name
+        # actor_hash = [first_name, last_name]
+        first_name + " " + last_name
+    end
+
+    def list_roles
+        self.characters.map do |role|
+            "#{role.name} - #{role.show.name}"
+            # binding.pry
+        end
+    end
+end 
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map do |actor|
+        actor.full_name
+    end
+    # binding.pry
+  end
+
+
 end
